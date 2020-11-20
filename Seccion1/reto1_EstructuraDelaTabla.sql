612928a4b6d691cd1a57bc8713cc72ab28fb5c4a
Your Name <you@example.com>
2020-11-19 23:45:42 -0700

    modificacionPrueba

--- Seccion1/reto1_EstructuraDelaTabla.sql
+++ Seccion1/reto1_EstructuraDelaTabla.sql
@@ -1,4 +1,4 @@
-#Usando la base de datos tienda, muestra la descripción de las tablas articulo, puesto y venta
+#Usando la base de datos tienda, muestra la descripción de las tablas articulo, puesto y ventaasxax
 use tienda;
 describe articulo;
 describe puesto;
